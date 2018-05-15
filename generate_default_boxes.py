0ed4b7c2bf28e28d08a0847785cd67f3e9af6355
GoDeng <dengzelu@188.com>
2018-05-15 20:52:46 +0800

    Update generate_default_boxes.py

--- generate_default_boxes.py
+++ generate_default_boxes.py
@@ -1,18 +1,18 @@
-def get_ctrs(x1, y1, x2, y2):
-    x_ctr = x1 + 0.5 * (x2 - x1)
+def get_ctrs(y1, x1, y2, x2):
     y_ctr = y1 + 0.5 * (y2 - y1)
-    return x_ctr, y_ctr
+    x_ctr = x1 + 0.5 * (x2 - x1)
+    return y_ctr, x_ctr
 
 
-def make_box(w, h, x_ctr, y_ctr):
-    x1 = x_ctr - 0.5 * (w - 1)
+def generate_box(y_ctr, x_ctr, h, w):
     y1 = y_ctr - 0.5 * (h - 1)
-    x2 = x1 + w - 1
+    x1 = x_ctr - 0.5 * (w - 1)
     y2 = y1 + h - 1
-    return x1, y1, x2, y2
+    x2 = x1 + w - 1
+    return y1, x1, y2, x2
 
 
-def make_feature_boxes(feature_shape, feature_stride, scales):
+def generate_feature_boxes(feature_shape, feature_stride, scales):
     feature_height = feature_shape[0]
     feature_width = feature_shape[1]
     feature_boxes = []
@@ -20,32 +20,34 @@ def make_feature_boxes(feature_shape, feature_stride, scales):
     for i in range(feature_height):
         for j in range(feature_width):
             for s in scales:
-                x1 = j * feature_stride
                 y1 = i * feature_stride
-                x2 = (j + 1) * feature_stride - 1
+                x1 = j * feature_stride
                 y2 = (i + 1) * feature_stride - 1
-                x_ctr, y_ctr = get_ctrs(x1, y1, x2, y2)
-                box = make_box(s, s, x_ctr, y_ctr)
+                x2 = (j + 1) * feature_stride - 1
+                y_ctr, x_ctr = get_ctrs(y1, x1, y2, x2)
+                box = generate_box(y_ctr, x_ctr, s, s)
                 feature_boxes.append(box)
-                
+
     return feature_boxes
 
 
-def make_default_boxes(config):
+def generate_default_boxes(config):
     image_shape = config.IMAGE_SHAPE
-    assert image_shape[0] // (2**8) == int(image_shape[0] / (2**8)) and \
-        image_shape[1] // (2**8) == int(image_shape[1] / (2**8))
-    
+    assert image_shape[0] // (2 ** 8) == int(image_shape[0] / (2 ** 8)) and \
+        image_shape[1] // (2 ** 8) == int(image_shape[1] / (2 ** 8))
+
     all_scales = config.ALL_SCALES
     assert len(all_scales) == 7
-    
+
     default_boxes = []
-    
+
     for i in range(2, 9):
-        feature_stride = 2**i
-        feature_shape = []
-        feature_shape.append(image_shape[0] // feature_stride)
-        feature_shape.append(image_shape[1] // feature_stride)
-        feature_boxes = make_feature_boxes(feature_shape, feature_stride, all_scales[i-2])
+        feature_stride = 2 ** i
+        feature_height = image_shape[0] // feature_stride
+        feature_width = image_shape[1] // feature_stride
+        feature_shape = [feature_height, feature_width]
+        feature_boxes = generate_feature_boxes(feature_shape, feature_stride, all_scales[i - 2])
         default_boxes.extend(feature_boxes)
+
     return default_boxes
+
